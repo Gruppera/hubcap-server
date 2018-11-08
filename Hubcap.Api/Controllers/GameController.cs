6fed698a725fddc45ef34a34d5fbafc75a0b8034
Daniel Bohlin <daniel.bohlin@gruppera.se>
2018-11-08 19:21:51 +0100

    add lobby functionality

--- Hubcap.Api/Controllers/GameController.cs
+++ Hubcap.Api/Controllers/GameController.cs
@@ -1,22 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hubcap.Game.Reversi;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Hubcap.Api.Controllers
 {
+    public class Game
+    {
+        public Guid Player1 { get; set; }
+        public Guid Player2 { get; set; }
+        public object Board { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class GameController : ControllerBase
     {
+        private static Dictionary<Guid, Game> _db = new Dictionary<Guid, Game>();
+
         [HttpGet]
-        public ActionResult<Guid> IWantToPlay()
+        [Route("iwannaplay")]
+        public ActionResult<Guid> IWantToPlay(Guid playerKey)
         {
-            return Guid.NewGuid();
+            return GetGame(playerKey);
+        }
+
+        Guid GetGame(Guid playerKey)
+        {
+            var game = _db.Where(x => x.Value.Player2 == Guid.Empty).ToList();
+
+            if (!game.Any())
+            {
+                var gameKey = Guid.NewGuid();
+                var g = new Game { Board = new Reversi().GetInitialState(), Player1 = playerKey };
+                _db.Add(gameKey, g);
+                return gameKey;
+            }
+
+            var g2 = game.First();
+            g2.Value.Player2 = playerKey;
+
+            return g2.Key;
         }
 
         [HttpGet]
+        [Route("get")]
         public ActionResult<string> GetNextMove(Guid gameKey)
         {
-            return "value";
+            _db.TryGetValue(gameKey, out var game);
+
+            return JsonConvert.SerializeObject(game);
         }
 
         [HttpGet]
---
+++ Hubcap.Game.Reversi/Reversi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hubcap.Game.Reversi
+{
+    public class Reversi
+    {
+        public char[,] GetInitialState()
+        {
+            //var tmp = new char[8,8];
+            var tmp = new char[,]
+            {
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'X', 'O', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'O', 'X', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+            };
+
+            return tmp;
+        }
+    }
+}
