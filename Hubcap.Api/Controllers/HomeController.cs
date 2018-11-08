09344808eeecdae1d45df27b9b872aa6fdea2ecf
Daniel Bohlin <daniel.bohlin@gruppera.se>
2018-11-08 18:33:14 +0100

    add webpage

---
+++ Hubcap.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hubcap.Api.Controllers
+{
+    [Controller]
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
