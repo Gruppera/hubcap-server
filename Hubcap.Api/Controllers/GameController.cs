83ce1fc77367d7b2c97cfdc8f6ed0f63585b42cc
Daniel Bohlin <daniel.bohlin@gruppera.se>
2018-11-08 18:41:12 +0100

    create game api

---
+++ Hubcap.Api/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hubcap.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Guid> IWantToPlay()
+        {
+            return Guid.NewGuid();
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetNextMove(Guid gameKey)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public ActionResult<string> MakeMove(Guid gameKey, string moveData)
+        {
+            return "Move";
+        }
+    }
+}
