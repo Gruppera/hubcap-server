c37ea32d48b7d04dcbe5a4516427fa8104ac7add
Daniel Bohlin <daniel.bohlin@gruppera.se>
2018-11-09 00:08:45 +0100

    have working validation

--- Hubcap.Game.Reversi/Reversi.cs
+++ Hubcap.Game.Reversi/Reversi.cs
@@ -2,12 +2,11 @@
 
 namespace Hubcap.Game.Reversi
 {
-    public class Reversi
+    public static class Reversi
     {
         public static char[,] GetInitialState()
         {
-            //var tmp = new char[8,8];
-            var tmp = new char[,]
+            var tmp = new[,]
             {
                 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -26,78 +25,45 @@ namespace Hubcap.Game.Reversi
         {
             if (board[x, y] != ' ') throw new InvalidOperationException();
 
-            var otherDisk = 'X';
-            if (disc == 'X')
-                otherDisk = 'O';
+            var otherDisk = disc == 'X' ? 'O' : 'X';
 
-            bool? ok = null;
             for (var horizontal = -1; horizontal < 2; horizontal++)
             {
                 for(var vertical = -1; vertical < 2; vertical++)
                 {
-                    if (horizontal != 0 || vertical != 0)
-                    {
-                        var ix = x + horizontal;
-                        var iy = y + vertical;
-                        ok = null;
-                        while (ix < 8 && ix >= 0 && iy < 8 && iy >= 0)
-                        {
-                            if (board[ix, iy] == otherDisk && (ok == null || ok == true))
-                                ok = true;
-                            if (board[ix, iy] == disc && ok == true)
-                                break;
-                            if (board[ix, iy] == disc && ok == null)
-                            {
-                                ok = false;
-                                break;
-                            }
+                    if (horizontal == 0 && vertical == 0) continue;
 
-                            ix = ix + horizontal;
-                            iy = iy + vertical;
+                    var ix = x + horizontal;
+                    var iy = y + vertical;
+                    string state = null;
+                    while (ix < 8 && ix >= 0 && iy < 8 && iy >= 0)
+                    {
+                        if (board[ix, iy] == otherDisk && state == null)
+                        {
+                            state = "opponent";
+                        }
+                        else if (board[ix, iy] == disc && state == "opponent")
+                        {
+                            state = "close";
+                        }
+                        else if (board[ix, iy] == disc && state == null)
+                        {
+                            state = "fail";
                         }
 
-                        if (ok != null && ok == true)
-                            return board;
+                        if (state != "opponent")
+                            break;
+
+                        ix = ix + horizontal;
+                        iy = iy + vertical;
                     }
-                    //else if (horizontal != 0)
-                    //{
-                    //    //Check horizontal
-                    //    for (var i = x + horizontal; i < 8 && i > 0; i = i + horizontal)
-                    //    {
-                    //        if (board[i, y] == otherDisk && (ok == null || ok == true))
-                    //            ok = true;
-                    //        if (board[i, y] == disc && ok == true)
-                    //            break;
-                    //        if (board[i, y] == disc && ok == null)
-                    //        {
-                    //            ok = false;
-                    //            break;
-                    //        }
-                    //    }
-                    //}
-                    //else if (vertical != 0)
-                    //{
-                    //    //Check vertical
-                    //    for (var i = y + vertical; i < 8 && i > 0; i = i + vertical)
-                    //    {
-                    //        if (board[x, i] == otherDisk && (ok == null || ok == true))
-                    //            ok = true;
-                    //        if (board[x, i] == disc && ok == true)
-                    //            break;
-                    //        if (board[x, i] == disc && ok == null)
-                    //        {
-                    //            ok = false;
-                    //            break;
-                    //        }
-                    //    }
-                    //}
+
+                    if (state == "close")
+                        return board;
                 }
             }
 
-            if (ok == null || ok == false)
-                throw new InvalidOperationException();
-
-            return board;
+            throw new InvalidOperationException();
         }
     }
 }
