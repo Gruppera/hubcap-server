40ebe1710e63b81a76b4e5a5a2bd526a54dee11e
Jonathan Engqvist <jonathan.engqvist@gmail.com>
2018-11-28 20:12:02 +0100

    move test classes to separate files

---
+++ Hubcap.Game.Reversi.Tests/ReversiCanMoveTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Hubcap.Game.Reversi.Tests
+{
+    [TestFixture]
+    public class ReversiCanMoveTests
+    {
+        [Test]
+        public void Place_X_on_initial_board()
+        {
+            // Arrange
+            var board = Reversi.GetInitialState();
+            var expected = new (int, int)[]
+            {
+                (4, 2),
+                (5, 3),
+                (2, 4),
+                (3, 5)
+            };
+
+            // Act
+            var res = Reversi.GetMoves(board, 'X');
+
+            // Assert
+            Assert.True(ArraysContentEqual(expected, res), "Expected and Result are not equal");
+
+        }
+
+        [Test]
+        public void Place_O_on_initial_board()
+        {
+            // Arrange
+            var board = Reversi.GetInitialState();
+            var expected = new (int, int)[]
+            {
+                (3,2),
+                (2,3),
+                (5,4),
+                (4,5)
+            };
+
+            // Act
+            var res = Reversi.GetMoves(board, 'O');
+
+            // Assert
+            Assert.True(ArraysContentEqual(expected, res), "Expected and Result are not equal");
+        }
+
+        [Test]
+        public void ArrayContentEqualTests()
+        {
+            var a1 = new (int, int)[] { (1,2), (3,4) };
+            var a2 = new (int, int)[] { (3,4), (1,2) };
+            Assert.True(ArraysContentEqual(a1, a2));
+
+            var b1 = new (int, int)[] { (1,2) };
+            var b2 = new (int, int)[] { };
+            Assert.False(ArraysContentEqual(b1, b2));
+
+            var c1 = new (int, int)[] { (1,2) };
+            var c2 = new (int, int)[] { (3,4) };
+            Assert.False(ArraysContentEqual(c1, c2));
+        }
+
+        private bool ArraysContentEqual((int,int)[] one, (int,int)[] two)
+        {
+            if (one.Length != two.Length)
+                return false;
+            if (one.Any(i2 => two.Any(x => x.Item1 == i2.Item1 && x.Item2 == i2.Item2)))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Hubcap.Game.Reversi.Tests/ReversiTurnTests.cs
@@ -0,0 +1,160 @@
+﻿using NUnit.Framework;
+
+namespace Hubcap.Game.Reversi.Tests
+{
+    [TestFixture]
+    public class ReversiTurnTests
+    {
+        [Test]
+        public void Placing_X_on_24_on_initial_board_turns_discs()
+        {
+            //Arrange
+            var board = Reversi.GetInitialState();
+            var expected = Reversi.GetInitialState();
+            expected[4, 2] = 'X';
+            expected[4, 3] = 'X';
+
+            //Act
+            var newBoard = Reversi.Move(board, 2, 4, 'X');
+
+            //Assert
+            Assert.That(newBoard, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Turn_initial_board()
+        {
+            //Arrange
+            var board = new[,]
+            {
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'X', 'O', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'O', 'X', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+            };
+            var expected = new[,]
+            {
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'X', 'O', ' ', ' ', ' '},
+                {' ', ' ', 'X', 'X', 'X', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+            };
+
+            //Act
+            var newBoard = Reversi.Move(board, 2, 4, 'X');
+
+            //Assert
+            Assert.That(newBoard, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Turn_matrix1()
+        {
+            //Arrange
+            var board = new[,]
+            {
+                {' ', 'O', 'O', 'O', 'O', 'O', 'O', 'X'},
+                {'O', 'O', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'O', ' ', 'O', ' ', ' ', ' ', ' ', ' '},
+                {'O', ' ', ' ', 'O', ' ', ' ', ' ', ' '},
+                {'O', ' ', ' ', ' ', 'O', ' ', ' ', ' '},
+                {'O', ' ', ' ', ' ', ' ', 'O', ' ', ' '},
+                {'O', ' ', ' ', ' ', ' ', ' ', 'O', ' '},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X'}
+            };
+            var expected = new[,]
+            {
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'X', ' ', 'X', ' ', ' ', ' ', ' ', ' '},
+                {'X', ' ', ' ', 'X', ' ', ' ', ' ', ' '},
+                {'X', ' ', ' ', ' ', 'X', ' ', ' ', ' '},
+                {'X', ' ', ' ', ' ', ' ', 'X', ' ', ' '},
+                {'X', ' ', ' ', ' ', ' ', ' ', 'X', ' '},
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X'}
+            };
+
+            //Act
+            var newBoard = Reversi.Move(board, 0, 0, 'X');
+
+            //Assert
+            Assert.That(newBoard, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Turn_matrix2()
+        {
+            //Arrange
+            var board = new[,]
+            {
+                {'X', ' ', ' ', ' ', 'X', ' ', ' ', ' '},
+                {' ', 'O', ' ', ' ', 'O', ' ', ' ', 'X'},
+                {' ', ' ', 'O', ' ', 'O', ' ', 'O', ' '},
+                {' ', ' ', ' ', 'O', 'O', 'O', ' ', ' '},
+                {'X', 'O', 'O', 'O', ' ', 'O', 'O', 'X'},
+                {' ', ' ', ' ', 'O', 'O', 'O', ' ', ' '},
+                {' ', ' ', 'O', ' ', 'O', ' ', 'O', ' '},
+                {' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X'},
+            };
+            var expected = new[,]
+            {
+                {'X', ' ', ' ', ' ', 'X', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X'},
+                {' ', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
+                {' ', ' ', ' ', 'X', 'X', 'X', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', ' ', 'X', 'X', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
+                {' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X'},
+            };
+
+            //Act
+            var newBoard = Reversi.Move(board, 4, 4, 'X');
+
+            //Assert
+            Assert.That(newBoard, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Turn_matrix3()
+        {
+            //Arrange
+            var board = new[,]
+            {
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {' ', 'O', ' ', ' ', ' ', ' ', ' ', 'O'},
+                {' ', ' ', 'O', ' ', ' ', ' ', ' ', 'O'},
+                {' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O'},
+                {' ', ' ', ' ', ' ', 'O', ' ', ' ', 'O'},
+                {' ', ' ', ' ', ' ', ' ', 'O', ' ', 'O'},
+                {' ', ' ', ' ', ' ', ' ', ' ', 'O', 'O'},
+                {'X', 'O', 'O', 'O', 'O', 'O', 'O', ' '},
+            };
+            var expected = new[,]
+            {
+                {'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {' ', 'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+                {' ', ' ', 'X', ' ', ' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', ' ', 'X', ' ', ' ', 'X'},
+                {' ', ' ', ' ', ' ', ' ', 'X', ' ', 'X'},
+                {' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X'},
+                {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+            };
+
+            //Act
+            var newBoard = Reversi.Move(board, 7, 7, 'X');
+
+            //Assert
+            Assert.That(newBoard, Is.EqualTo(expected));
+        }
+    }
+}
