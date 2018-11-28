1348224a6e9121438ad14d1308b7a8ab6ff32659
Jonathan Engqvist <jonathan.engqvist@gmail.com>
2018-11-28 20:10:22 +0100

    add function to get possible moves on the board

--- Hubcap.Game.Reversi.Tests/ReversiValidationTests.cs
+++ Hubcap.Game.Reversi.Tests/ReversiValidationTests.cs
@@ -1,8 +1,80 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Hubcap.Game.Reversi.Tests
 {
+    [TestFixture]
+    public class ReversiCanMoveTests
+    {
+        [Test]
+        public void Place_X_on_initial_board()
+        {
+            // Arrange
+            var board = Reversi.GetInitialState();
+            var expected = new (int, int)[]
+            {
+                (4, 2),
+                (5, 3),
+                (2, 4),
+                (3, 5)
+            };
+
+            // Act
+            var res = Reversi.GetMoves(board, 'X');
+
+            // Assert
+            Assert.True(ArraysContentEqual(expected, res), "Expected and Result are not equal");
+
+        }
+
+        [Test]
+        public void Place_O_on_initial_board()
+        {
+            // Arrange
+            var board = Reversi.GetInitialState();
+            var expected = new (int, int)[]
+            {
+                (3,2),
+                (2,3),
+                (5,4),
+                (4,5)
+            };
+
+            // Act
+            var res = Reversi.GetMoves(board, 'O');
+
+            // Assert
+            Assert.True(ArraysContentEqual(expected, res), "Expected and Result are not equal");
+        }
+
+        [Test]
+        public void ArrayContentEqualTests()
+        {
+            var a1 = new (int, int)[] { (1,2), (3,4) };
+            var a2 = new (int, int)[] { (3,4), (1,2) };
+            Assert.True(ArraysContentEqual(a1, a2));
+
+            var b1 = new (int, int)[] { (1,2) };
+            var b2 = new (int, int)[] { };
+            Assert.False(ArraysContentEqual(b1, b2));
+
+            var c1 = new (int, int)[] { (1,2) };
+            var c2 = new (int, int)[] { (3,4) };
+            Assert.False(ArraysContentEqual(c1, c2));
+        }
+
+        private bool ArraysContentEqual((int,int)[] one, (int,int)[] two)
+        {
+            if (one.Length != two.Length)
+                return false;
+            if (one.Any(i2 => two.Any(x => x.Item1 == i2.Item1 && x.Item2 == i2.Item2)))
+                return true;
+
+            return false;
+        }
+    }
+
     [TestFixture]
     public class ReversiTurnTests
     {
@@ -172,7 +244,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 3, 3, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -197,7 +269,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 2, 3, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -210,7 +282,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 0, 0, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -223,7 +295,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 7, 7, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -248,7 +320,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 5, 4, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -285,7 +357,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 3, 2, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -298,7 +370,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 4, 5, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -311,7 +383,7 @@ namespace Hubcap.Game.Reversi.Tests
             void newBoard() => Reversi.Move(board, 2, 5, 'X');
 
             //Assert
-            Assert.Throws<InvalidOperationException>(newBoard);
+            Assert.Throws<ReversiException>(newBoard);
         }
 
         [Test]
@@ -322,9 +394,10 @@ namespace Hubcap.Game.Reversi.Tests
             board[2,5] = 'X';
 
             //Act
-            Reversi.Move(board, 2, 5, 'X');
+            void newBoard() => Reversi.Move(board, 2, 5, 'X');
 
             //Assert
+            Assert.Throws<ReversiException>(newBoard);
         }
     }
 }
--- Hubcap.Game.Reversi/Reversi.cs
+++ Hubcap.Game.Reversi/Reversi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hubcap.Game.Reversi
 {
@@ -32,6 +33,10 @@ namespace Hubcap.Game.Reversi
 
         public static char[,] Move(char[,] board, int x, int y, char disc)
         {
+            var validMoves = GetMoves(board, disc);
+            if (!validMoves.Contains((x,y)))
+                throw new ReversiException("Invalid move!");
+
             TurnDisks(board, x, y, disc);
 
             //TODO: Make move
@@ -90,6 +95,66 @@ namespace Hubcap.Game.Reversi
 
             if (!ok)
                 throw new ReversiException("Invalid move.");
+        }
+
+        public static (int x, int y)[] GetMoves(char[,] board, char disc)
+        {
+            var otherDisk = disc == 'X' ? 'O' : 'X';
+            var validMoves = new List<(int,int)>();
+
+            for (var horizontal = -1; horizontal < 2; horizontal++)
+            {
+                for (var vertical = -1; vertical < 2; vertical++)
+                {
+                    if (horizontal == 0 && vertical == 0)
+                        continue;
+
+                    // Checking all the positions on the board
+                    for (var x = 0; x < 8; x++)
+                    {
+                        for (var y = 0; y < 8; y++)
+                        {
+                            var line = new List<char>();
+                            (int x, int y) temp = (x, y);
+
+                            do
+                            {
+                                var c = board[temp.x, temp.y];
+                                line.Add(c);
+                                temp.x += horizontal;
+                                temp.y += vertical;
+                            }
+                            while (InBounds(temp.x, temp.y));
+
+                            if (line[0] != ' ')
+                                continue;
+
+                            if (line.Count == 1)
+                                continue;
+
+                            if (line.Count >= 2 && line[1] != otherDisk)
+                                continue;
+
+                            if (line.Count >= 3)
+                            {
+                                line.RemoveRange(0,2);
+                                if (!line.Contains(disc))
+                                    continue;
+                            }
+
+                            // If we get here the x,y is valid to place disc on
+                            validMoves.Add((x, y));
+                        }
+                    }
+                }
+            }
+
+            return validMoves.ToArray();
+
+            bool InBounds(int x, int y)
+            {
+                return x >= 0 && x < 8 && y >= 0 && y < 8;
+            }
         }
     }
 }
